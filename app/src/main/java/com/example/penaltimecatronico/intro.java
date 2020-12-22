15b3de4c4e1a67d9d88a44dfa17ddef2e7b34db8
napilco <napilco@espol.edu.ec>
2020-12-22 00:42:15 -0500

    colocando logo

---
+++ app/src/main/java/com/example/penaltimecatronico/intro.java
@@ -0,0 +1,26 @@
+package com.example.penaltimecatronico;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import java.util.Timer;
+import java.util.TimerTask;
+import android.content.Intent;
+
+public class intro extends AppCompatActivity {
+    Timer timer;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+        timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(intro.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },4000);
+    }
+}
