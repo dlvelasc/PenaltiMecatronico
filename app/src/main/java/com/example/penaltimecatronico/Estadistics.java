05c4591b84677f7d33f8ba112e56d2c9688c1a97
Dome <dlvelasc@espol.edu.ec>
2021-01-19 09:14:44 -0500

    Actualizacion de tablas

--- app/src/main/java/com/example/penaltimecatronico/Estadistics.java
+++ app/src/main/java/com/example/penaltimecatronico/Estadistics.java
@@ -3,6 +3,7 @@ package com.example.penaltimecatronico;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -26,16 +27,18 @@ import java.util.HashMap;
 public class Estadistics extends AppCompatActivity {
     FirebaseDatabase rootNode;
     DatabaseReference reference;
-
-    ArrayList<Sesion> values;
+TableLayout scores;
+Estadistics con;
+ArrayList<Sesion> sesions;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.estadistics);
-        values = new ArrayList<>();
+        initTable();
         getData();
-        populateTable();
+        sesions=new ArrayList<>();
+
 
 
 
@@ -43,67 +46,66 @@ public class Estadistics extends AppCompatActivity {
 
     public void getData() {
         rootNode = FirebaseDatabase.getInstance();// obtener valores de el nodo principal
-        reference = rootNode.getReference().child("users").child(LogInClient.activeUser).child("sesiones");
-        reference.addListenerForSingleValueEvent(new ValueEventListener() {
+        rootNode.getReference().child("users").child(LogInClient.activeUser).child("sesiones").addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
-                for (DataSnapshot d : snapshot.getChildren()) {
-
-                    Sesion s = d.getValue(Sesion.class);
-                    values.add(new Sesion(s.getFecha(),s.getGoles(),s.getIntentos()));
-                }
+                for (DataSnapshot sn: snapshot.getChildren()){
+                    Sesion s=sn.getValue(Sesion.class);
+                            addToTable(s);
+  }
             }
+
 
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
             }
         });
     }
+public void initTable(){
+    scores = (TableLayout) findViewById(R.id.scores);
+    scores.setStretchAllColumns(true);
+    scores.bringToFront();
+    TableRow encabezado= new TableRow(getApplicationContext());
+    TextView fecha=new TextView(getApplicationContext());
+    fecha.setText("Fecha");
+    encabezado.addView(fecha);
+    TextView goles=new TextView(getApplicationContext());
+    goles.setText("Goles");
+    encabezado.addView(goles);
+    TextView tiros=new TextView(getApplicationContext());
+    tiros.setText("Tiros");
+    encabezado.addView(tiros);
+    TextView puntos=new TextView(getApplicationContext());
+    puntos.setText("Puntos");
+    encabezado.addView(puntos);
+    TextView acierto=new TextView(getApplicationContext());
+    acierto.setText("% acierto");
+    encabezado.addView(acierto);
+    scores.addView(encabezado);
 
-    public void populateTable() {
-
-        TableLayout scores = (TableLayout) findViewById(R.id.scores);
-        scores.setStretchAllColumns(true);
-        scores.bringToFront();
-        TableRow encabezado= new TableRow(this);
-        TextView fecha=new TextView(this);
-        fecha.setText("Fecha");
-        encabezado.addView(fecha);
-        TextView goles=new TextView(this);
-        goles.setText("Goles");
-        encabezado.addView(goles);
-        TextView tiros=new TextView(this);
-        tiros.setText("Tiros");
-        encabezado.addView(tiros);
-        TextView puntos=new TextView(this);
-        puntos.setText("Puntos");
-        encabezado.addView(puntos);
-        TextView acierto=new TextView(this);
-        acierto.setText("% acierto");
-        encabezado.addView(acierto);
-        scores.addView(encabezado);
-        for (Sesion s : values){
-                TableRow tr=new TableRow(this);
-                TextView date=new TextView(this);
-                date.setText(s.getFecha());
-                TextView goals=new TextView(this);
-                goals.setText(s.getGoles());
-                TextView shots=new TextView(this);
-                shots.setText(s.getIntentos());
-                TextView points=new TextView(this);
-                points.setText(String.valueOf(s.getGoles()*1.1));
-                TextView accuracy=new TextView(this);
-                accuracy.setText(String.valueOf(s.getGoles()/s.getIntentos()));
-                tr.addView(date);
-                tr.addView(goals);
-                tr.addView(shots);
-                tr.addView(points);
-                tr.addView(accuracy);
-                scores.addView(tr);
-        }
+}
 
 
-    }
+public void addToTable(Sesion s){
+    TableRow tr = new TableRow(getApplicationContext());
+    TextView date = new TextView(getApplicationContext());
+    date.setText(s.getFecha().split("T")[0]);
+    TextView goals = new TextView(getApplicationContext());
+    goals.setText(String.valueOf(s.getGoles()));
+    TextView shots = new TextView(getApplicationContext());
+    shots.setText(String.valueOf(s.getIntentos()));
+    TextView points = new TextView(getApplicationContext());
+    points.setText(String.valueOf(s.getGoles() * 1.1));
+    TextView accuracy = new TextView(getApplicationContext());
+    accuracy.setText(String.valueOf(s.getGoles() / s.getIntentos()));
+    tr.addView(date);
+    tr.addView(goals);
+    tr.addView(shots);
+    tr.addView(points);
+    tr.addView(accuracy);
+    scores.addView(tr);
+
+}
 
 
     public void botMenuEstadist(View v) {
